a73ff7fa376aaaaeb00b1f0487c3c0131f09b48c
SamTebbet <samtebbet@gmail.com>
2024-02-04 19:38:06 +0000

    Fixed stupid bug with navbar

--- static/modals.js
+++ static/modals.js
@@ -1,21 +1,22 @@
 $(document).on('click', '.login_button', function () {
-        $.ajax({
-            type: 'GET',
-            url: 'login',
-            success: function (output) {
-            $('#modalcontainer').html(output).modal('show');//now its working
-            },
-            error: function(output){
-            alert("fail");
-            }
-        });
+    $.ajax({
+        type: 'GET',
+        url: 'login',
+        success: function (output) {
+        $('#modalcontainer').html(output).modal('show');//now its working
+        },
+        error: function(output){
+        alert("fail");
+        }
     });
+});
+
 $(document).on('click', '.register_button', function () {
     $.ajax({
         type: 'GET',
         url: 'register',
         success: function (output) {
-            $('#modalcontainer').html(output).modal('show');//now its working
+        $('#modalcontainer').html(output).modal('show');//now its working
         },
         error: function(output){
         alert("fail");
