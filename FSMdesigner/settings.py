fe596c9272626ba4abd45c03a76190b30f732d6f
Sam Tebbet <samtebbet@gmail.com>
2024-03-26 15:51:55 +0000

    Secret Keys properly implemented

--- FSMdesigner/settings.py
+++ FSMdesigner/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_*5_^(pcxr+h3dg0e+@l+=^jqd^772a3+s_f^@hlx4ki1yds=+'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
